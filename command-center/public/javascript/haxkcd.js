b9574d182e7715635208e6acba01e1f6888de879
Anish Athalye <me@anishathalye.com>
2016-07-14 18:04:18 -0700

    Implement custom commands

--- command-center/public/javascript/haxkcd.js
+++ command-center/public/javascript/haxkcd.js
@@ -14,6 +14,76 @@ function randomChoice(items) {
 	return items[getRandomInt(0, items.length-1)];
 }
 
+var xkcd = {
+	last: null,
+	cache: {},
+	base: 'http://dynamic.xkcd.com/api-0/jsonp/comic/',
+
+	get: function(num, success, error) {
+		if (num == null) {
+			path = '';
+		} else if (Number(num)) {
+			path = String(num);
+		} else {
+			error(false);
+			return false;
+		}
+
+		if (num in this.cache) {
+			this.last = this.cache[num];
+			success(this.cache[num]);
+		} else {
+			return $.ajax({
+				url: this.base+path,
+				dataType: 'jsonp',
+				success: $.proxy(function(data) {
+					this.last = this.cache[num] = data;
+					success(data);
+				}, this),
+				error: error});
+		}
+	}
+};
+
+var xkcdDisplay = function(terminal, path) {
+	function fail() {
+		terminal.print($('<p>').addClass('error').text('display: unable to open image "'+path+'": No such file or directory.'));
+		terminal.setWorking(false);
+	}
+
+	if (path) {
+		path = String(path);
+		num = Number(path.match(/^\d+/));
+		filename = pathFilename(path);
+	} else {
+		num = xkcd.last.num;
+	}
+
+	terminal.setWorking(true);
+    xkcd.get(num, function(data) {
+        if (!filename || (filename == pathFilename(data.img))) {
+            $('<img>')
+                .hide()
+                .load(function() {
+                    terminal.print($('<h3>').text(data.num+": "+data.title));
+                    $(this).fadeIn();
+
+                    var comic = $(this);
+                    if (data.link) {
+                        comic = $('<a>').attr('href', data.link).append($(this));
+                    }
+                    terminal.print(comic);
+
+                    terminal.setWorking(false);
+                })
+                .attr({src:data.img, alt:data.title, title:data.alt})
+                .addClass('comic');
+        } else {
+            fail();
+        }
+    }, fail);
+};
+
 TerminalShell.commands['start'] = function(terminal) {
 	terminal.setWorking(true);
 	$.ajax({
@@ -100,6 +170,15 @@ TerminalShell.commands['submit'] = function(terminal) {
 		success: function(data) {
 			if (typeof data.message !== 'undefined') {
 				terminal.print(data.message);
+				if (data.message.indexOf('incident') !== -1 && getRandomInt(0, 5) < 1) {
+					terminal.setWorking(true);
+					setTimeout(function() {
+						xkcdDisplay(terminal, 838);
+						// ^ calls `terminal.setWorking(false)` when it's done
+					}, 3000);
+				} else {
+					terminal.setWorking(false);
+				}
 			} else if (typeof data.correct !== 'undefined') {
 				if (data.correct === true) {
 					terminal.print('Correct!');
@@ -109,10 +188,11 @@ TerminalShell.commands['submit'] = function(terminal) {
 						'Try again.',
 					]));
 				}
+				terminal.setWorking(false);
 			} else {
 				terminal.print("Something went wrong... :|");
+				terminal.setWorking(false);
 			}
-			terminal.setWorking(false);
 		},
 		error: function(e) {
 			terminal.print("Something went wrong... :|");
@@ -145,112 +225,6 @@ TerminalShell.commands['finish'] = function(terminal, email) {
 	});
 };
 
-var xkcd = {
-	latest: null,
-	last: null,
-	cache: {},
-	base: 'http://dynamic.xkcd.com/api-0/jsonp/comic/',
-
-	get: function(num, success, error) {
-		if (num == null) {
-			path = '';
-		} else if (Number(num)) {
-			path = String(num);
-		} else {
-			error(false);
-			return false;
-		}
-
-		if (num in this.cache) {
-			this.last = this.cache[num];
-			success(this.cache[num]);
-		} else {
-			return $.ajax({
-				url: this.base+path,
-				dataType: 'jsonp',
-				success: $.proxy(function(data) {
-					this.last = this.cache[num] = data;
-					success(data);
-				}, this),
-				error: error});
-		}
-	}
-};
-
-var xkcdDisplay = TerminalShell.commands['display'] = function(terminal, path) {
-	function fail() {
-		terminal.print($('<p>').addClass('error').text('display: unable to open image "'+path+'": No such file or directory.'));
-		terminal.setWorking(false);
-	}
-
-	if (path) {
-		path = String(path);
-		num = Number(path.match(/^\d+/));
-		filename = pathFilename(path);
-
-		if (num > xkcd.latest.num) {
-			terminal.print("Time travel mode not enabled.");
-			return;
-		}
-	} else {
-		num = xkcd.last.num;
-	}
-
-	terminal.setWorking(true);
-    xkcd.get(num, function(data) {
-        if (!filename || (filename == pathFilename(data.img))) {
-            $('<img>')
-                .hide()
-                .load(function() {
-                    terminal.print($('<h3>').text(data.num+": "+data.title));
-                    $(this).fadeIn();
-
-                    var comic = $(this);
-                    if (data.link) {
-                        comic = $('<a>').attr('href', data.link).append($(this));
-                    }
-                    terminal.print(comic);
-
-                    terminal.setWorking(false);
-                })
-                .attr({src:data.img, alt:data.title, title:data.alt})
-                .addClass('comic');
-        } else {
-            fail();
-        }
-    }, fail);
-};
-
-TerminalShell.commands['next'] = function(terminal) {
-	xkcdDisplay(terminal, xkcd.last.num+1);
-};
-
-TerminalShell.commands['previous'] =
-TerminalShell.commands['prev'] = function(terminal) {
-	xkcdDisplay(terminal, xkcd.last.num-1);
-};
-
-TerminalShell.commands['first'] = function(terminal) {
-	xkcdDisplay(terminal, 1);
-};
-
-TerminalShell.commands['latest'] =
-TerminalShell.commands['last'] = function(terminal) {
-	xkcdDisplay(terminal, xkcd.latest.num);
-};
-
-TerminalShell.commands['random'] = function(terminal) {
-	xkcdDisplay(terminal, getRandomInt(1, xkcd.latest.num));
-};
-
-TerminalShell.commands['goto'] = function(terminal, subcmd) {
-	$('#screen').one('cli-ready', function(e) {
-		terminal.print('Did you mean "display"?');
-	});
-	xkcdDisplay(terminal, 292);
-};
-
-
 TerminalShell.commands['sudo'] = function(terminal) {
 	var cmd_args = Array.prototype.slice.call(arguments);
 	cmd_args.shift(); // terminal
@@ -281,6 +255,19 @@ TerminalShell.filters.push(function (terminal, cmd) {
 		return cmd;
 	}
 });
+
+// boy is this one going to be annoying
+TerminalShell.filters.push(function (terminal, cmd) {
+	if (getRandomInt(0, 10) < 1) {
+		return '__segfault';
+	} else {
+		return cmd;
+	}
+});
+
+TerminalShell.commands['__segfault'] = function(terminal) {
+	terminal.print('Segmentation fault (core dumped)');
+}
 
 TerminalShell.commands['shutdown'] = TerminalShell.commands['poweroff'] = function(terminal) {
 	if (this.sudo) {
@@ -359,10 +346,8 @@ Filesystem = {
 		});
 	}}
 };
-Filesystem['blog'] = Filesystem['blag'] = linkFile('http://blag.xkcd.com');
-Filesystem['forums'] = Filesystem['fora'] = linkFile('http://forums.xkcd.com/');
-Filesystem['store'] = linkFile('http://store.xkcd.com/');
-Filesystem['about'] = linkFile('http://xkcd.com/about/');
+Filesystem['hackmit'] = linkFile('https://hackmit.org');
+Filesystem['register'] = linkFile('https://my.hackmit.org/');
 TerminalShell.pwd = Filesystem;
 
 TerminalShell.commands['cd'] = function(terminal, path) {
@@ -377,7 +362,6 @@ TerminalShell.commands['cd'] = function(terminal, path) {
 	}
 };
 
-TerminalShell.commands['dir'] =
 TerminalShell.commands['ls'] = function(terminal, path) {
 	var name_list = $('<ul>');
 	$.each(this.pwd, function(name, obj) {
@@ -396,18 +380,10 @@ TerminalShell.commands['cat'] = function(terminal, path) {
 		} else if (this.pwd[path].type == 'dir') {
 			terminal.print('cat: '+path+': Is a directory');
 		}
-	} else if (pathFilename(path) == 'alt.txt') {
-		terminal.setWorking(true);
-		num = Number(path.match(/^\d+/));
-		xkcd.get(num, function(data) {
-			terminal.print(data.alt);
-			terminal.setWorking(false);
-		}, function() {
-			terminal.print($('<p>').addClass('error').text('cat: "'+path+'": No such file or directory.'));
-			terminal.setWorking(false);
-		});
+	} else if (typeof path !== 'undefined') {
+		terminal.print($('<p>').addClass('error').text('cat: "'+path+'": No such file or directory.'));
 	} else {
-		terminal.print('You\'re a kitty!');
+		terminal.print('Meow!');
 	}
 };
 
@@ -436,94 +412,6 @@ TerminalShell.commands['rm'] = function(terminal, flags, path) {
 	}
 };
 
-TerminalShell.commands['cheat'] = function(terminal) {
-	terminal.print($('<a>').text('*** FREE SHIPPING ENABLED ***').attr('href', 'http://store.xkcd.com/'));
-};
-
-TerminalShell.commands['reddit'] = function(terminal, num) {
-	num = Number(num);
-	if (num) {
-		url = 'http://xkcd.com/'+num+'/';
-	} else {
-		var url = window.location;
-	}
-	terminal.print($('<iframe src="http://www.reddit.com/static/button/button1.html?width=140&url='+encodeURIComponent(url)+'&newwindow=1" height="22" width="140" scrolling="no" frameborder="0"></iframe>'));
-};
-
-TerminalShell.commands['wget'] = TerminalShell.commands['curl'] = function(terminal, dest) {
-	if (dest) {
-		terminal.setWorking(true);
-		var browser = $('<div>')
-			.addClass('browser')
-			.append($('<iframe>')
-					.attr('src', dest).width("100%").height(600)
-					.one('load', function() {
-						terminal.setWorking(false);
-					}));
-		terminal.print(browser);
-		return browser;
-	} else {
-		terminal.print("Please specify a URL.");
-	}
-};
-
-TerminalShell.commands['write'] =
-TerminalShell.commands['irc'] = function(terminal, nick) {
-	if (nick) {
-		$('.irc').slideUp('fast', function() {
-			$(this).remove();
-		});
-		var url = "http://widget.mibbit.com/?server=irc.foonetic.net&channel=%23xkcd";
-		if (nick) {
-			url += "&nick=" + encodeURIComponent(nick);
-		}
-		TerminalShell.commands['curl'](terminal, url).addClass('irc');
-	} else {
-		terminal.print('usage: irc <nick>');
-	}
-};
-
-TerminalShell.commands['unixkcd'] = function(terminal, nick) {
-	TerminalShell.commands['curl'](terminal, "http://www.xkcd.com/unixkcd/");
-};
-
-TerminalShell.commands['apt-get'] = function(terminal, subcmd) {
-	if (!this.sudo && (subcmd in {'update':true, 'upgrade':true, 'dist-upgrade':true})) {
-		terminal.print('E: Unable to lock the administration directory, are you root?');
-	} else {
-		if (subcmd == 'update') {
-			terminal.print('Reading package lists... Done');
-		} else if (subcmd == 'upgrade') {
-			if (($.browser.name == 'msie') || ($.browser.name == 'firefox' && $.browser.versionX < 3)) {
-				terminal.print($('<p>').append($('<a>').attr('href', 'http://abetterbrowser.org/').text('To complete installation, click here.')));
-			} else {
-				terminal.print('This looks pretty good to me.');
-			}
-		} else if (subcmd == 'dist-upgrade') {
-			var longNames = {'win':'Windows', 'mac':'OS X', 'linux':'Linux'};
-			var name = $.os.name;
-			if (name in longNames) {
-				name = longNames[name];
-			} else {
-				name = 'something fancy';
-			}
-			terminal.print('You are already running '+name+'.');
-		} else if (subcmd == 'moo') {
-			terminal.print('        (__)');
-			terminal.print('        (oo)');
-			terminal.print('  /------\\/ ');
-			terminal.print(' / |    ||  ');
-			terminal.print('*  /\\---/\\  ');
-			terminal.print('   ~~   ~~  ');
-			terminal.print('...."Have you mooed today?"...');
-		} else if (!subcmd) {
-			terminal.print('This APT has Super Cow Powers.');
-		} else {
-			terminal.print('E: Invalid operation '+subcmd);
-		}
-	}
-};
-
 function oneLiner(terminal, msg, msgmap) {
 	if (msgmap.hasOwnProperty(msg)) {
 		terminal.print(msgmap[msg]);
@@ -534,119 +422,7 @@ function oneLiner(terminal, msg, msgmap) {
 }
 
 TerminalShell.commands['man'] = function(terminal, what) {
-	pages = {
-		'last': 'Man, last night was AWESOME.',
-		'help': 'Man, help me out here.',
-		'next': 'Request confirmed; you will be reincarnated as a man next.',
-		'cat':  'You are now riding a half-man half-cat.'
-	};
-	if (!oneLiner(terminal, what, pages)) {
-		terminal.print('Oh, I\'m sure you can figure it out.');
-	}
-};
-
-TerminalShell.commands['locate'] = function(terminal, what) {
-	keywords = {
-		'ninja': 'Ninja can not be found!',
-		'keys': 'Have you checked your coat pocket?',
-		'joke': 'Joke found on user.',
-		'problem': 'Problem exists between keyboard and chair.',
-		'raptor': 'BEHIND YOU!!!'
-	};
-	if (!oneLiner(terminal, what, keywords)) {
-		terminal.print('Locate what?');
-	}
-};
-
-Adventure = {
-	rooms: {
-		0:{description:'You are at a computer using unixkcd.', exits:{west:1, south:10}},
-		1:{description:'Life is peaceful there.', exits:{east:0, west:2}},
-		2:{description:'In the open air.', exits:{east:1, west:3}},
-		3:{description:'Where the skies are blue.', exits:{east:2, west:4}},
-		4:{description:'This is what we\'re gonna do.', exits:{east:3, west:5}},
-		5:{description:'Sun in wintertime.', exits:{east:4, west:6}},
-		6:{description:'We will do just fine.', exits:{east:5, west:7}},
-		7:{description:'Where the skies are blue.', exits:{east:6, west:8}},
-		8:{description:'This is what we\'re gonna do.', exits:{east:7}},
-		10:{description:'A dark hallway.', exits:{north:0, south:11}, enter:function(terminal) {
-				if (!Adventure.status.lamp) {
-					terminal.print('You are eaten by a grue.');
-					Adventure.status.alive = false;
-					Adventure.goTo(terminal, 666);
-				}
-			}
-		},
-		11:{description:'Bed. This is where you sleep.', exits:{north:10}},
-		666:{description:'You\'re dead!'}
-	},
-
-	status: {
-		alive: true,
-		lamp: false
-	},
-
-	goTo: function(terminal, id) {
-		Adventure.location = Adventure.rooms[id];
-		Adventure.look(terminal);
-		if (Adventure.location.enter) {
-			Adventure.location.enter(terminal);
-		}
-	}
-};
-Adventure.location = Adventure.rooms[0];
-
-TerminalShell.commands['look'] = Adventure.look = function(terminal) {
-	terminal.print(Adventure.location.description);
-	if (Adventure.location.exits) {
-		terminal.print();
-
-		var possibleDirections = [];
-		$.each(Adventure.location.exits, function(name, id) {
-			possibleDirections.push(name);
-		});
-		terminal.print('Exits: '+possibleDirections.join(', '));
-	}
-};
-
-TerminalShell.commands['go'] = Adventure.go = function(terminal, direction) {
-	if (Adventure.location.exits && direction in Adventure.location.exits) {
-		Adventure.goTo(terminal, Adventure.location.exits[direction]);
-	} else if (!direction) {
-		terminal.print('Go where?');
-	} else if (direction == 'down') {
-		terminal.print("On our first date?");
-	} else {
-		terminal.print('You cannot go '+direction+'.');
-	}
-};
-
-TerminalShell.commands['light'] = function(terminal, what) {
-	if (what == "lamp") {
-		if (!Adventure.status.lamp) {
-			terminal.print('You set your lamp ablaze.');
-			Adventure.status.lamp = true;
-		} else {
-			terminal.print('Your lamp is already lit!');
-		}
-	} else {
-		terminal.print('Light what?');
-	}
-};
-
-TerminalShell.commands['sleep'] = function(terminal, duration) {
-	duration = Number(duration);
-	if (!duration) {
-		duration = 5;
-	}
-	terminal.setWorking(true);
-	terminal.print("You take a nap.");
-	$('#screen').fadeOut(1000);
-	window.setTimeout(function() {
-		terminal.setWorking(false);
-		$('#screen').fadeIn();
-		terminal.print("You awake refreshed.");
-	}, 1000*duration);
+	terminal.print('RTFM!');
 };
 
 // No peeking!
@@ -663,71 +439,23 @@ TerminalShell.commands['help'] = TerminalShell.commands['halp'] = function(termi
 
 TerminalShell.fallback = function(terminal, cmd) {
 	oneliners = {
-		'make me a sandwich': 'What? Make it yourself.',
-		'make love': 'I put on my robe and wizard hat.',
-		'i read the source code': '<3',
-		'pwd': 'You are in a maze of twisty passages, all alike.',
-		'lpr': 'PC LOAD LETTER',
-		'hello joshua': 'How about a nice game of Global Thermonuclear War?',
-		'xyzzy': 'Nothing happens.',
-		'date': 'March 32nd',
-		'hello': 'Why hello there!',
-		'who': 'Doctor Who?',
-		'xkcd': 'Yes?',
-		'su': 'God mode activated. Remember, with great power comes great ... aw, screw it, go have fun.',
-		'fuck': 'I have a headache.',
+		'hack': 'No hacking allowed!',
 		'whoami': 'You are Richard Stallman.',
-		'nano': 'Seriously? Why don\'t you just use Notepad.exe? Or MS Paint?',
-		'top': 'It\'s up there --^',
-		'moo':'moo',
-		'ping': 'There is another submarine three miles ahead, bearing 225, forty fathoms down.',
-		'find': 'What do you want to find? Kitten would be nice.',
-		'hello':'Hello.','more':'Oh, yes! More! More!',
-		'your gay': 'Keep your hands off it!',
-		'hi':'Hi.','echo': 'Echo ... echo ... echo ...',
-		'bash': 'You bash your head against the wall. It\'s not very effective.','ssh': 'ssh, this is a library.',
-		'uname': 'Illudium Q-36 Explosive Space Modulator',
-		'finger': 'Mmmmmm...',
-		'kill': 'Terminator deployed to 1984.',
-		'use the force luke': 'I believe you mean source.',
-		'use the source luke': 'I\'m not luke, you\'re luke!',
-		'serenity': 'You can\'t take the sky from me.',
-		'enable time travel': 'TARDIS error: Time Lord missing.',
-		'ed': 'You are not a diety.'
+		'ssh': 'It\'s not secure enough!',
 	};
 	oneliners['emacs'] = 'You should really use vim.';
 	oneliners['vi'] = oneliners['vim'] = 'You should really use emacs.';
 
 	cmd = cmd.toLowerCase();
 	if (!oneLiner(terminal, cmd, oneliners)) {
-		if (cmd == "asl" || cmd == "a/s/l") {
+		if (cmd == "hint") {
 			terminal.print(randomChoice([
-				'2/AMD64/Server Rack',
-				'328/M/Transylvania',
-				'6/M/Battle School',
-				'48/M/The White House',
-				'7/F/Rapture',
-				'Exactly your age/A gender you\'re attracted to/Far far away.',
-				'7,831/F/Lothlórien',
-				'42/M/FBI Field Office'
-			]));
-		} else if  (cmd == "hint") {
-			terminal.print(randomChoice([
- 				'We offer some really nice polos.',
- 				$('<p>').html('This terminal will remain available at <a href="http://xkcd.com/unixkcd/">http://xkcd.com/unixkcd/</a>'),
+ 				'Here\'s a hint: QGXYVVWQ2',
  				'Use the source, Luke!',
- 				'There are cheat codes.'
+				'There are cheat codes.',
+				'No.',
  			]));
-		} else if (cmd == 'find kitten') {
-			terminal.print($('<iframe width="800" height="600" src="http://www.robotfindskitten.net/rfk.swf"></iframe>'));
-		} else if (cmd == 'buy stuff') {
-			Filesystem['store'].enter();
-		} else if (cmd == 'time travel') {
-			xkcdDisplay(terminal, 630);
-		} else if (/:\(\)\s*{\s*:\s*\|\s*:\s*&\s*}\s*;\s*:/.test(cmd)) {
-			Terminal.setWorking(true);
 		} else {
-			$.get("/unixkcd/missing", {cmd: cmd});
 			return false;
 		}
 	}
